0dd090660b3f548f68dface0f268f6a8acd70eca
André Almeida <a.alves.de.almeida@avanade.com>
2019-10-04 17:36:57 -0300

    retornar url do thumbnail

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -12,6 +14,7 @@ mongoose.connect('mongodb+srv://andre:andre@cluster0-fuqbj.mongodb.net/aircnc?re
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 const port = process.env.PORT || 3333;
